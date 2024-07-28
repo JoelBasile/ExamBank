e01c7c4c0955fe0e73a95a2022ac0396af76f954
JoelBasile <joelbasile111@gmail.com>
2024-07-28 19:46:35 +1000

    Bug fix - uploading image

--- src/ProfilePage/index.tsx
+++ src/ProfilePage/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { uploadData } from 'aws-amplify/storage';
+import { Authenticator } from '@aws-amplify/ui-react'
 
 
 function ProfilePage() {
@@ -11,19 +12,24 @@ function ProfilePage() {
 
 
     return (
-        <div>
-            <input type="file" onChange={handleChange} />
-            <button
-                onClick={() =>
-                    uploadData({
-                        path: `picture-submissions/${file.name}`,
-                        data: file,
-                    })
-                }
-            >
-                Upload
-            </button>
-        </div>
+        <Authenticator>
+            {({ user }) => (
+                <div>
+                    <input type="file" onChange={handleChange} />
+                    <button disabled={!file}
+                        onClick={() =>
+                        { if (file) {
+                            uploadData({
+                                path: `picture-submissions/${file}`,
+                                data: file,
+                            })}
+                        }}
+                    >
+                        Upload
+                    </button>
+                </div>
+            )}
+        </Authenticator>
     );
 };
 
